374d96a6e0e450f2c9f5824e4b7151782137d0ff
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-10-24 16:30:01 +0900

    練習問題１．変数aに100、変数bに30を代入し、その積、商、和、差を求めて表示するプログラムEX3_Calc.javaを作成しました

---
+++ src/main/java/EX3_Calc/EX3_Calc.java
@@ -0,0 +1,10 @@
+public class EX3_Calc {
+    public static void main(String[] args) {
+        int a = 100,b = 30;                  // 変数a,bの宣言と初期値の代入
+
+        System.out.println("100 * 30 =" + (a * b) );
+        System.out.println("100 / 30 =" + ((double)(a) / b));
+        System.out.println("100 + 30 =" + (a + b));
+        System.out.println("100 - 30 =" + (a - b));
+    }
+}
